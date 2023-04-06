2f2858b0d59759f6842a890fff8c30556521585b
Matt Chowning <mchowning@gmail.com>
2023-04-05 20:41:33 -0400

    Wear app

---
+++ app/src/main/java/com/mattchowning/myapplication/presentation/MainActivity.kt
@@ -0,0 +1,97 @@
+/* While this template provides a good starting point for using Wear Compose, you can always
+ * take a look at https://github.com/android/wear-os-samples/tree/main/ComposeStarter and
+ * https://github.com/android/wear-os-samples/tree/main/ComposeAdvanced to find the most up to date
+ * changes to the libraries and their usages.
+ */
+
+package com.mattchowning.myapplication.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.Button
+import androidx.wear.compose.material.Text
+import androidx.wear.compose.navigation.composable
+import androidx.wear.compose.navigation.rememberSwipeDismissableNavController
+import com.google.android.horologist.compose.navscaffold.WearNavScaffold
+import com.google.android.horologist.compose.pager.PagerScreen
+import com.mattchowning.myapplication.presentation.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+
+    @OptIn(ExperimentalFoundationApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                PagerScreen(2) { page ->
+                    when (page) {
+                        0 -> {
+                            val navController = rememberSwipeDismissableNavController()
+                            WearNavScaffold(
+                                startDestination = "start",
+                                navController = navController
+                            ) {
+
+                                composable("start") {
+                                    Box(
+                                        contentAlignment = Alignment.Center,
+                                        modifier = Modifier.fillMaxSize()
+                                    ) {
+                                        Column(
+                                            horizontalAlignment = Alignment.CenterHorizontally,
+                                            verticalArrangement = Arrangement.spacedBy(8.dp),
+                                            modifier = Modifier.fillMaxWidth()
+                                        ) {
+                                            Text("Page 1")
+                                            Button(
+                                                onClick = { navController.navigate("subpage") }
+                                            ) {
+                                                Text("Navigate to subpage")
+                                            }
+                                        }
+                                    }
+                                }
+
+                                composable("subpage") {
+                                    ScreenWithText("Subpage")
+                                }
+                            }
+                        }
+
+                        1 -> ScreenWithText("Page 2")
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun ScreenWithText(text: String) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(text)
+    }
+}
+
+
+//@Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
+//@Composable
+//fun DefaultPreview() {
+//    WearApp("Preview Android")
+//}
